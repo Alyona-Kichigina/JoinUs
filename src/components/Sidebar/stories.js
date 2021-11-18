27e408a0734be620ce59195b4e2490f1ef69dd30
Antonov <alexander.antonov@publicismedia.com>
2021-11-18 17:20:34 +0500

    Добавил Storybook.

---
+++ src/components/Sidebar/stories.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Sidebar = () => (
+    <div>
+        iqwjoiqjwoqwoije
+    </div>
+)
+
+export default {
+    title: "component/sidebar",
+    component: Sidebar
+}
+
+const Template = (args) => <Sidebar {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+};
