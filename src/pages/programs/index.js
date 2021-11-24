88c0d2cd1f6133a9de3c04ffacf73f8bd5b48d9c
Antonov <alexander.antonov@publicismedia.com>
2021-11-24 23:47:43 +0500

    Начал работу над таблицей списков.

--- src/pages/programs/index.js
+++ src/pages/programs/index.js
@@ -1,47 +1,6 @@
 import React, {Component} from 'react';
 import NavContentBtn from "../../components/NavContentButton";
 import { CONTENT_LINKS } from "../../components/Constants"
-import AppList from "../../components/AppList";
-
-const programsList = [
-    {
-        name: "Прграмма для разработчиков",
-        time: "3 месяца",
-        comment: "Программа для начинающих разработчиков"
-    },
-    {
-        name: "Программа для аналитиков",
-        time: "2 месяца",
-        comment: "-",
-        inside: [
-            {
-                name: "Программа для разработчиков",
-                time: "3 месяца",
-                comment: "Программа для начинающих разработчиков"
-            },
-            {
-                name: "Программа для разработчиков",
-                time: "3 месяца",
-                comment: "-"
-            },
-            {
-                name: "Программа для руководителей",
-                time: "3 месяца",
-                comment: "-"
-            }
-        ]
-    },
-    {
-        name: "Прграмма для руководителей",
-        time: "3 месяца",
-        comment: "-"
-    }
-]
-const settings = [
-    {
-
-    }
-]
 
 class Programs extends Component {
     render() {
@@ -66,10 +25,6 @@ class Programs extends Component {
                     Programms Programms Programms Programms Programms
                     Programms Programms Programms Programms Programms
                     Programms Programms Programms Programms Programms
-                    <AppList
-                        settings={settings}
-                        data={programsList}
-                    />
                 </div>
             </div>
         );
