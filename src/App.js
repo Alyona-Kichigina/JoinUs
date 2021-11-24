8a876d065feb2ecb41e350bc45bb33d731291923
Antonov <alexander.antonov@publicismedia.com>
2021-11-24 17:41:32 +0500

    \o

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import NavContent from "./components/NavContent"
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-        <NavContent />
-    </div>
+      <BrowserRouter>
+          <div className="App">
+              <NavContent />
+          </div>
+      </BrowserRouter>
   );
 }
 
--- src/pages/levels/index.js
+++ src/pages/levels/index.js
@@ -1,27 +1,47 @@
 import React, {Component} from 'react';
 import NavContentBtn from "../../components/NavContentButton";
 import { CONTENT_LINKS } from "../../components/Constants"
+import Breadcrumbs from "../../components/Breadcrumbs";
+
+const pageData = {
+    pageName: "Программа для разработчиков"
+}
 
 class Levels extends Component {
     render() {
+        const { history } = this.props
         return (
             <div className="h-full">
-                <NavContentBtn
-                    links={CONTENT_LINKS}
+                <div>
+                    <div>
+                        <Breadcrumbs
+                            {...this.props}
+                        />
+                    </div>
+                    <div className="flex justify-between mb-6 mt-4">
+                        <div>
+                            { pageData.pageName }
+                        </div>
+                    </div>
+                    <div>
+                        <NavContentBtn
+                            links={CONTENT_LINKS}
 
-                />
-                 <div className="bg-white h-full">
-                     Levels Levels Levels Levels Levels Levels Levels
-                     Levels Levels Levels Levels Levels Levels Levels
-                     Levels Levels Levels Levels Levels Levels Levels
-                     Levels Levels Levels Levels Levels Levels Levels
-                     Levels Levels Levels Levels Levels Levels Levels
-                     Levels Levels Levels Levels Levels Levels Levels
-                     Levels Levels Levels Levels Levels Levels Levels
-                     Levels Levels Levels Levels Levels Levels Levels
-                     Levels Levels Levels Levels Levels Levels Levels
-                     Levels Levels Levels Levels Levels Levels Levels
-                 </div>
+                        />
+                         <div className="bg-white h-full">
+                             Levels Levels Levels Levels Levels Levels Levels
+                             Levels Levels Levels Levels Levels Levels Levels
+                             Levels Levels Levels Levels Levels Levels Levels
+                             Levels Levels Levels Levels Levels Levels Levels
+                             Levels Levels Levels Levels Levels Levels Levels
+                             Levels Levels Levels Levels Levels Levels Levels
+                             Levels Levels Levels Levels Levels Levels Levels
+                             Levels Levels Levels Levels Levels Levels Levels
+                             Levels Levels Levels Levels Levels Levels Levels
+                             Levels Levels Levels Levels Levels Levels Levels
+                         </div>
+                    </div>
+                </div>
             </div>
         );
     }
