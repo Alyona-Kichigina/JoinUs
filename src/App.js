45d2aa0a7a606ba7b1a300c690706a5b44c8727e
Bacca <forinventorwork@mail.ru>
2021-12-07 11:41:21 +0300

    +

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import NavigationDrawer from "@Components/NavigationDrawer";
 import Header from "@Components/Header";
 import NavContent from "./components/NavContent"
+import { BrowserRouter } from "react-router-dom";
 import "@Styles/style.css"
 import "@Styles/fonts.css"
 import "@Styles/animation.css"
